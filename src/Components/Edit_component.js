d965e7dda8d595a7454743f2b5415c97be82885b
prathap-singh <113010828+prathap-singh@users.noreply.github.com>
2023-04-27 12:06:22 +0530

    updated code

--- src/Components/Edit_component.js
+++ src/Components/Edit_component.js
@@ -1,106 +1,81 @@
-import React, { useEffect, useState } from 'react';
-import { useParams, useNavigate } from 'react-router-dom';
-import LocalStorage from './storage.js';
-import contactsData from './contactsData.js';
+import React, { useState, useEffect } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+import LocalStorage from "./storage";
 
-const EditContact = () => {
-  const { id } = useParams();
+const EditContact = (props) => {
+  const location = useLocation();
   const navigate = useNavigate();
-  const [contact, setContact] = useState(LocalStorage.getContacts(id));
+  const [contact, setContact] = useState({});
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    const updatedContact = {
-      ...contact,
-      firstName: event.target.firstName.value,
-      lastName: event.target.lastName.value,
-      email: event.target.email.value,
-      phoneNumber: event.target.phoneNumber.value,
-      address: event.target.address.value,
-      city: event.target.city.value,
-      state: event.target.state.value,
-      country: event.target.country.value,
-      postalCode: event.target.postalCode.value,
-    };
-    LocalStorage.updateContact(updatedContact);
-    navigate('/');
-  };
+  useEffect(() => {
+    const id = location.pathname.split("/")[2];
+    const storedContacts = LocalStorage.getContacts();
+    const selectedContact = storedContacts.find(
+      (contact) => contact.id === id
+    );
+    setContact(selectedContact);
+  }, [location]);
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
-    setContact((prevContact) => ({
-      ...prevContact,
-      [name]: value,
-    }));
+    setContact({ ...contact, [name]: value });
   };
 
-  useEffect(() => {
-    let storedContacts = LocalStorage.getContacts();
-    if (storedContacts.length === 0) {
-      LocalStorage.setContacts(contactsData);
-      storedContacts = contactsData;
-    }
-    const contact = storedContacts.find(c => c.id === parseInt(id));
-    if (contact) {
-      setContact(contact);
-    }
-  }, [id]);
-
-
-
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const storedContacts = LocalStorage.getContacts();
+    const updatedContacts = storedContacts.map((c) =>
+      c.id === contact.id ? contact : c
+    );
+    LocalStorage.setContacts(updatedContacts);
+    navigate("/");
+  };
 
   return (
-    <>
-      <div className='container'>
-
-        <div className='text-center'>
-          <h1>Contact Edit Form</h1>
+    <div className="container">
+      <h1>Edit Contact</h1>
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label htmlFor="firstName">First Name</label>
+          <input type="text" className="form-control" id="firstName" name="firstName" value={contact.firstName} onChange={handleInputChange} />
         </div>
-        <div className='p-5'>
-
-          <form onSubmit={handleSubmit}>
-            <div >
-              <label htmlFor="firstName">First Name:</label>
-              <input className="form-control" type="text" id="firstName" name="firstName" defaultValue={contact.firstName} onChange={handleInputChange} />
-            </div>
-            <div className='form-group mt-2'>
-              <label htmlFor="lastName">Last Name:</label>
-              <input className="form-control" type="text" id="lastName" name="lastName" defaultValue={contact.lastName} onChange={handleInputChange} />
-            </div>
-            <div className='form-group mt-2'>
-              <label htmlFor="email">Email:</label>
-              <input className="form-control" type="email" id="email" name="email" defaultValue={contact.email} onChange={handleInputChange} />
-            </div>
-            <div className='form-group mt-2'>
-              <label htmlFor="phoneNumber">Phone Number:</label>
-              <input className="form-control" type="tel" id="phoneNumber" name="phoneNumber" defaultValue={contact.phoneNumber} onChange={handleInputChange} />
-            </div>
-            <div className='form-group mt-2'>
-              <label htmlFor="address">Address:</label>
-              <input className="form-control" type="text" id="address" name="address" defaultValue={contact.address} onChange={handleInputChange} />
-            </div>
-            <div className='form-group mt-2'>
-              <label htmlFor="address">City</label>
-              <input className="form-control" type="text" id="city" name="city" defaultValue={contact.city} onChange={handleInputChange} />
-            </div>
-            <div className='form-group mt-2'>
-              <label htmlFor="address">State</label>
-              <input className="form-control" type="text" id="state" name="state" defaultValue={contact.state} onChange={handleInputChange} />
-            </div>
-            <div className='form-group mt-2'>
-              <label htmlFor="address">Country</label>
-              <input className="form-control" type="text" id="country" name="country" defaultValue={contact.country} onChange={handleInputChange} />
-            </div>
-            <div className='form-group mt-2'>
-              <label htmlFor="address">Postal Code</label>
-              <input className="form-control" type="text" id="postalCode" name="postalCode" defaultValue={contact.postalCode} onChange={handleInputChange} />
-            </div>
-            <button type="submit" className="btn btn-primary mt-3">Update</button>
-          </form>
+        <div className="form-group">
+          <label htmlFor="lastName">Last Name</label>
+          <input type="text" className="form-control"  id="lastName" name="lastName" value={contact.lastName} onChange={handleInputChange} />
         </div>
-      </div>
-    </>
-  );
+        <div className="form-group">
+          <label htmlFor="email">Email</label>
+          <input  type="email" className="form-control" id="email" name="email" value={contact.email}  onChange={handleInputChange}  />
+        </div>
+        <div className="form-group">
+          <label htmlFor="phoneNumber">Phone Number</label>
+          <input  type="text" className="form-control" id="phoneNumber" name="phoneNumber" value={contact.phoneNumber}  onChange={handleInputChange}  />
+        </div>
+        <div className="form-group">
+          <label htmlFor="address">Address</label>
+          <input  type="text" className="form-control" id="address" name="address" value={contact.address}  onChange={handleInputChange}  />
+        </div>
+        <div className="form-group">
+          <label htmlFor="city">City</label>
+          <input  type="text" className="form-control" id="city" name="city" value={contact.city}  onChange={handleInputChange}  />
+        </div>
+        <div className="form-group">
+          <label htmlFor="state">State</label>
+          <input  type="text" className="form-control" id="state" name="state" value={contact.state}  onChange={handleInputChange}  />
+        </div>
+        <div className="form-group">
+          <label htmlFor="country">Country</label>
+          <input  type="text" className="form-control" id="country" name="country" value={contact.country}  onChange={handleInputChange}  />
+        </div>
+        <div className="form-group">
+          <label htmlFor="postalCode">PostalCode</label>
+          <input  type="text" className="form-control" id="postalCode" name="postalCode" value={contact.postalCode}  onChange={handleInputChange}  />
+        </div>
+        <button type="submit" className="btn btn-primary mt-3">Submit</button>
+        </form>
+        </div>
+  )
 };
 
-export default EditContact;
+
+export default EditContact
